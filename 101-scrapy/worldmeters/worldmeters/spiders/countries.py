d703ba8f521a1a7dae57417bd05ced0fcc556f1c
Robert <kyp001@yahoo.com.tw>
2022-12-06 17:09:21 +0800

    update item 101 countries.py(for xpath)

--- 101-scrapy/worldmeters/worldmeters/spiders/countries.py
+++ 101-scrapy/worldmeters/worldmeters/spiders/countries.py
@@ -4,7 +4,14 @@ import scrapy
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['www.worldometers.info']
-    start_urls = ['https://www.worldometers.info/']
+    # start_urls = ['https://www.worldometers.info/']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        pass
+        title = response.xpath("//h1/text()").get()
+        countries = response.xpath("//td/a/text()").getall()
+
+        yield {
+          'tittle': title,
+          'counties': countries
+        }
