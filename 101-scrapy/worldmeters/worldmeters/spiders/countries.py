9cf6d85e74825562566b9eff78cc0754d567fc5d
Robert <kyp001@yahoo.com.tw>
2022-12-09 17:15:45 +0800

    update  item 101 countries.py(for country's year/population)

--- 101-scrapy/worldmeters/worldmeters/spiders/countries.py
+++ 101-scrapy/worldmeters/worldmeters/spiders/countries.py
@@ -1,17 +1,35 @@
 import scrapy
-
+import logging
 
 class CountriesSpider(scrapy.Spider):
-    name = 'countries'
-    allowed_domains = ['www.worldometers.info']
-    # start_urls = ['https://www.worldometers.info/']
-    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+  name = 'countries'
+  allowed_domains = ['www.worldometers.info']
+  # start_urls = ['https://www.worldometers.info/']
+  start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
-    def parse(self, response):
-        title = response.xpath("//h1/text()").get()
-        countries = response.xpath("//td/a/text()").getall()
+  def parse(self, response):
+    countries = response.xpath("//td/a")
+    for country in countries:
+      name = country.xpath(".//text()").get()
+      link = country.xpath(".//@href").get()
 
-        yield {
-          'tittle': title,
-          'counties': countries
-        }
+      # absolute url
+      # absolute_url = f'https://www.worldometers.info{link}'
+      # absolute_url = response.urljoin(link)
+      # yield scrapy.Request(url=absolute_url)
+
+      # relative url
+      yield response.follow(url=link, callback=self.parse_country)
+
+  def parse_country(self, response):
+    # show log
+    # logging.info(response.url)
+    rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+    for row in rows:
+      year = row.xpath("./td[1]/text()").get()
+      population = row.xpath("./td[2]/strong/text()").get()
+      yield {
+        'year' : year,
+        'population': population
+      }
+
