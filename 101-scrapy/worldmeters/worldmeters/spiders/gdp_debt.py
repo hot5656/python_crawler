ea970cf88f2b3381a691fe5a44228a95a7909e45
Robert <kyp001@yahoo.com.tw>
2022-12-13 17:10:24 +0800

    1. update 101 spider gdp_dept 2. create 101 project tinydeal

--- 101-scrapy/worldmeters/worldmeters/spiders/gdp_debt.py
+++ 101-scrapy/worldmeters/worldmeters/spiders/gdp_debt.py
@@ -4,7 +4,16 @@ import scrapy
 class GdpDebtSpider(scrapy.Spider):
     name = 'gdp_debt'
     allowed_domains = ['worldpopulationreview.com']
-    start_urls = ['http://worldpopulationreview.com/']
+    # start_urls = ['http://worldpopulationreview.com/']
+    start_urls = ['https://worldpopulationreview.com/country-rankings/countries-by-national-debt/']
 
     def parse(self, response):
-        pass
+        rows = response.xpath("//tbody/tr")
+        for row in rows:
+            name = row.xpath("./td[1]/a/text()").get()
+            debt_rate = row.xpath("./td[2]/text()").get()
+
+            yield {
+                'country_name' : name,
+                'debt_rate' : debt_rate
+            }
