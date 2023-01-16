35bea69e54faeff049b952db570fd45e34692541
Robert <kyp001@yahoo.com.tw>
2023-01-16 22:29:29 +0800

    item 109 project foootball modify

--- 109-scrapy-practice2/foootball/foootball/spiders/news.py
+++ 109-scrapy-practice2/foootball/foootball/spiders/news.py
@@ -1,12 +1,19 @@
 import scrapy
+import time
 
 
 class NewsSpider(scrapy.Spider):
     name = 'news'
     allowed_domains = ['foootball.cc']
-    start_urls = ['https://foootball.cc/']
+    # start_urls = ['https://foootball.cc/']
+    # start_urls = ['https://foootball.cc/?page=2']
     index = 0
     MAX_NEWS = 75
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'}
+
+    def start_requests(self):
+        url = 'https://foootball.cc'
+        yield scrapy.Request(url=url, callback=self.parse, headers=self.headers)
 
     def parse(self, response):
         news = response.xpath("(//h2[@class='h2-title topic-line-four'])[2]/following::div/div[@class='row']")
@@ -27,10 +34,12 @@ class NewsSpider(scrapy.Spider):
 
         next_page =  response.xpath("//a[@rel='next']/@href").get()
         if next_page or self.index < self.MAX_NEWS:
+            time.sleep(5)
             print("=========================")
             print("--"+next_page+"--")
             # print(response.urljoin(next_page))
-            yield scrapy.Request(url=next_page, callback=self.parse)
+            # yield scrapy.Request(url=next_page, callback=self.parse, headers=self.headers, dont_filter=True)
+            yield scrapy.Request(url=next_page, callback=self.parse, headers=self.headers)
 
 
         # (//h2[@class='h2-title topic-line-four'])[2]/following::div/div[@class='row']
